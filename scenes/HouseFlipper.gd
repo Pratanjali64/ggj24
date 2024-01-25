ea08ed644343fd305508330d5efe8cd4aa82f163
Riley Lane <rileylanea@gmail.com>
2024-01-24 17:21:13 -0800

    Add Cargo Worker

--- scenes/HouseFlipper.gd
+++ scenes/HouseFlipper.gd
@@ -2,14 +2,14 @@ extends Minigame
 
 @onready var timer: Timer = $WinConditionTimer
 
-func _on_win_condition_area_entered(area):
-	timer.start()
-
-
-func _on_win_condition_area_exited(area):
-	timer.stop()
-
-
 func _on_win_condition_timer_timeout():
 	timer.stop()
 	winAnimation.play("success");
+
+
+func _on_win_condition_2_area_entered(area):
+	timer.start()
+
+
+func _on_win_condition_2_area_exited(area):
+	timer.stop()
