ffb3f03c66a0980fbddf10e38b13399a4efc1e1f
Riley Lane <rileylanea@gmail.com>
2024-01-28 11:06:03 -0800

    House textures

---
+++ scenes/House.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var rot = (360 + int(rotation_degrees)) % 360;
+	print_debug(str(rot));
+	if (rot > 25 and rot < 90):
+		if (!animationPlayer.is_playing()):
+			animationPlayer.play("teeter_right")
+	elif (rot > 270 and rot < 335):
+		if (!animationPlayer.is_playing()):
+			animationPlayer.play("teeter_left")
+	else:
+		if (!animationPlayer.is_playing()):
+			animationPlayer.play("idle")
